9e9b8a9114e64b72b3ca08c2e21365f2e436809e
ediretorvitor-droid <ediretorvitor@gmail.com>
2026-09-08 18:00:11 -0300

    Use integrated app search and history logic

--- app/src/main/java/com/topdorama/app/MainActivity.java
+++ app/src/main/java/com/topdorama/app/MainActivity.java
@@ -12,9 +12,6 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class MainActivity extends Activity {
     private WebView webView;
 
@@ -47,27 +44,8 @@ public class MainActivity extends Activity {
                 try { startActivity(i); } catch (Exception ignored) {}
                 return true;
             }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                if (url != null && url.startsWith("file:///android_asset/index.html")) {
-                    injectAssetScript(view, "pt-search.js");
-                }
-            }
         });
         webView.loadUrl("file:///android_asset/index.html");
-    }
-
-    private void injectAssetScript(WebView view, String assetName) {
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open(assetName), "UTF-8"));
-            StringBuilder js = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) js.append(line).append('\n');
-            reader.close();
-            view.evaluateJavascript(js.toString(), null);
-        } catch (Exception ignored) {}
     }
 
     @Override
